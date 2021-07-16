6bb2217169916c38ada0e90d153ec15fc7fa8adc
James Sherlock <15193942+Sherlouk@users.noreply.github.com>
2021-07-16 09:10:09 +0100

    Add Swift Package Manager support (#3)

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ios-accessibility-text-snapshot",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "ios-accessibility-text-snapshot",
+            targets: ["ios-accessibility-text-snapshot"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            name: "SnapshotTesting",
+            url: "https://github.com/pointfreeco/swift-snapshot-testing.git",
+            .upToNextMajor(from: "1.8.0")
+        )
+    ],
+    targets: [
+        .target(
+            name: "ios-accessibility-text-snapshot",
+            dependencies: ["SnapshotTesting"],
+            path: "Sources"
+        )
+    ]
+)
